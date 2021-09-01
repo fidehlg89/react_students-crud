f1394b851ed364fe41163126f20df4e783fe4a79
Fidel Ernesto <fidehlg89@gmail.com>
2021-09-01 15:12:46 -0400

    Continue modifying

--- src/pages/Group/groups.js
+++ src/pages/Group/groups.js
@@ -1,207 +1,207 @@
-import React from "react"
-import { FaPen, FaTrash } from 'react-icons/fa'
-import Form from '../../components/form'
-import Table from '../../components/table'
-import { groups, professors, students } from "./../../db/data"
+import React, { useState } from "react";
+import { FaPen, FaTrash } from "react-icons/fa";
+import Form from "../../components/form";
+import Table from "../../components/table";
+import { groups, professors, /*students*/ } from "./../../db/data";
 
-class GroupList extends React.Component {
+const GroupList = (props) => {
+  const [id, setId]=useState();
+  const [groupList, setGroups] = useState([groups]);
+  const [professorList, setProfessors] = useState(professors);
+  //const [studentList, setStudents] = useState(students);
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            groups: groups,
-            professors: professors,
-            students: students,
-            isCreating: false,
-            isUpdating: false,
-            professorOption: '',
-            groupProfesorId: 0
-        }
-        this.onNewGroup = this.onNewGroup.bind(this)
-    }
+  const [isCreating, setCreating] = useState(false);
+  const [isUpdating, setUpdating] = useState(false);
 
-    onCreateGroup = () => {
-        const { professors } = this.state
-        let professorlist = professors.map(item =>
-            <option value={item.id} key={item.id}>{item.name}</option>
-        )
-        this.setState({
-            professors: professorlist,
-            isCreating: !this.state.isCreating,
-        })
-    }
-    renderCreateview() {
-        const { professorOption, professors } = this.state
-        return <div className="group-item-create">
-            <h5>Crear Grupo</h5>
-            <Form>
-                <input type="text" placeholder="Nombre" ref="theTextNameInput" />
-                <div>
-                    <span>Seleccione Profesor: </span>
-                    <select className="" value={professorOption} ref="theTextProfessorInput" onChange={this.handleProfSelect}>{professors}</select>
-                </div>
-                <button
-                    onClick={this.onNewGroup}
-                    className="btn btn-success btn-sm">Guardar
-                </button>
-                <span> </span>
-                <button
-                    onClick={this.isCreating}
-                    className="btn btn-danger btn-sm">Cancelar
-                </button>
-            </Form>
-        </div>
-    }
-    onNewGroup = () => {  
-        this.setState({
-            groups: [
-                ...this.state.groups,
-                {
-                    id: Date.now(),
-                    name: this.refs.theTextNameInput.value,
-                    professorId: this.refs.theTextProfessorInput.value,
-                },
-            ],
-            isCreating: false,            
-            professors:professors
-        })
-    }
-    //Editando Grupos
-    onUpdateGroup = (i) => {
-        const { groups } = this.state
-        const index = groups.findIndex(n => n.id === i)
-        if (index === -1) {
-            return
-        }
-        let professorlist = professors.map(item =>
-            <option value={item.id} key={item.id}>{item.name}</option>
-        )
-        this.setState({
-            professors: professorlist,
-            isUpdating: !this.state.isUpdating,
-            id: index,
-        })
-    }
-    onEditGroup = () => {
-        const id = this.state.id
-        let group = this.state.groups[id]
-        group.name = this.refs.theTextNameInput.value
-        group.professorId = this.refs.theTextProfessorInput.value
-        this.setState({
-            professors: professors,
-            isUpdating: !this.state.isUpdating,
-        })
-    }
-    handleProfSelect = (e) => {
-        this.setState(
+  const [professorOption, setProfOptions] = useState("");
+  //const [groupProfesorId, setProfId] = useState(0);
+
+  const onCreateGroup = () => {
+    let professorlist = professorList.map((item) => (
+      <option value={item.id} key={item.id}>
+        {item.name}
+      </option>
+    ));
+    setProfessors(professorlist);
+    setCreating(!isCreating);
+  };
+  const renderCreateview = () => 
+    (
+      <div className="group-item-create">
+        <h5>Crear Grupo</h5>
+        <Form>
+          <input type="text" placeholder="Nombre" ref="theTextNameInput" />
+          <div>
+            <span>Seleccione Profesor: </span>
+            <select
+              value={professorOption}
+              ref="theTextProfessorInput"
+              onChange={handleProfSelect}
+            >
+              {professors}
+            </select>
+          </div>
+          <button onClick={onNewGroup} className="btn btn-success btn-sm">
+            Guardar
+          </button>
+          <span> </span>
+          <button onClick={isCreating} className="btn btn-danger btn-sm">
+            Cancelar
+          </button>
+        </Form>
+      </div>
+    );
+
+    const onNewGroup = () => {
+        setGroups([
+            ...groups,
             {
-                professorOption: e.target.value
-            }
-        )
+              id: Date.now(),
+              name: this.refs.theTextNameInput.value,
+              professorId: this.refs.theTextProfessorInput.value,
+            }])
+        setCreating(false);
+        setProfessors(professors);
+  };
+  //Editando Grupos
+  const onUpdateGroup = (i) => {
+    const index = groups.findIndex((n) => n.id === i);
+    if (index === -1) {
+      return;
     }
-    //Formulario de Edición
-    renderUpdateview = (id) => {
-        const group = this.state.groups[id]
-
-        const { professorOption, professors } = this.state
-        return <div className="group-item-create">
-            <h5>Editar Grupo</h5>
-            <Form>
-                <input
-                    type="text"
-                    placeholder="Nombre"
-                    ref="theTextNameInput"
-                    defaultValue={group.name}
-                />
+    let professorlist = professors.map((item) => (
+      <option value={item.id} key={item.id}>
+        {item.name}
+      </option>
+    ));
+    setProfessors(professorlist);
+    setUpdating(isUpdating);
+    setId(index);
+  };
+  const onEditGroup = () => {
+    let group = groupList[id];
+    group.name = this.refs.theTextNameInput.value;
+    group.professorId = this.refs.theTextProfessorInput.value;
+    setProfessors(professorList);
+    setUpdating(!isUpdating);
+  };
+  const handleProfSelect = (e) => {
+      setProfOptions(e.target.value);
+  };
+  //Formulario de Edición
+  const renderUpdateview = (id) => {
+    const group = groupList[id];
+    
+    return (
+      <div className="group-item-create">
+        <h5>Editar Grupo</h5>
+        <Form>
+          <input
+            type="text"
+            placeholder="Nombre"
+            ref="theTextNameInput"
+            defaultValue={group.name}
+          />
+          <div>
+            <span>Seleccione un Profesor: </span>
+            <select
+              defaultValue={professorOption}
+              ref="theTextProfessorInput"
+              onChange={handleProfSelect}
+            >
+              {professors}
+            </select>
+          </div>
+          <button onClick={onEditGroup} className="btn btn-success btn-sm">
+            Guardar
+          </button>
+          <span> </span>
+          <button className="btn btn-danger btn-sm">Cancelar</button>
+        </Form>
+      </div>
+    );
+  };
+  //Eliminando Grupo
+  const onDeleteGroup = (group) => {
+    const index = groups.findIndex((n) => n.id === group.id);
+    if (index === -1) {
+      return;
+    }
+    const newItems = groups.slice();
+    newItems.splice(index, 1);
+    setGroups(newItems);
+  };
+  //Vista Principal del CRUD Grupos
+  const renderDefaultview = () => {
+    let itemslist = groupList.map((item) => (
+      <tr key={item.id}>
+        <th>{item.name}</th>
+        <th>{professorList[item.professorId - 1].name}</th>
+        <th>
+          <div>
+            <button
+              type="submit"
+              className="btn btn-default btn-sm btn-success"
+              onClick={() => onUpdateGroup(item.id)}
+            >
+              <FaPen />
+            </button>
+            <span> </span>
+            <button
+              className="btn btn-default btn-sm btn-danger"
+              onClick={() => onDeleteGroup(item)}
+            >
+              <FaTrash />
+            </button>
+          </div>
+        </th>
+      </tr>
+    ));
+    return (
+      <div>
+        {
+          <section className="panel">
+            <h5 className="panel-heading">Listado de Grupos</h5>
+            <button
+              className="new-group-item btn btn-primary btn-sm"
+              onClick={onCreateGroup}
+            >
+              {" "}
+              Nuevo Grupo
+            </button>
+            <div className="panel-body">
+              {itemslist.length > 0 ? (
                 <div>
-                    <span>Seleccione un Profesor: </span>
-                    <select
-                        defaultValue={professorOption}
-                        ref="theTextProfessorInput"
-                        onChange={this.handleProfSelect}>
-                        {professors}
-                    </select>
+                  <Table>
+                    <thead className="text-info">
+                      <tr>
+                        <th>Grupo</th>
+                        <th>Profesor</th>
+                        <th>Acciones</th>
+                      </tr>
+                    </thead>
+                    <tbody>{itemslist}</tbody>
+                  </Table>
                 </div>
-                <button
-                    onClick={this.onEditGroup}
-                    className="btn btn-success btn-sm">
-                    Guardar
-                </button>
-                <span> </span>
-                <button
-                    className="btn btn-danger btn-sm">
-                    Cancelar
-                </button>
-            </Form>
-        </div>
-    }
-    //Eliminando Grupo
-    onDeleteGroup = (group) => {
-        const { groups } = this.state
-        const index = groups.findIndex(n => n.id === group.id)
-        if (index === -1) {
-            return
-        }
-        const newItems = groups.slice()
-        newItems.splice(index, 1)
-        this.setState({
-            groups: newItems
-        })
-    }
-    //Vista Principal del CRUD Grupos
-    renderDefaultview = () => {
-        const { groups, professors } = this.state
-        let itemslist = groups.map(item =>
-            <tr key={item.id}>
-                <th>{item.name}</th>
-                <th>{professors[item.professorId-1].name}</th>
-                <th className="">
-                    <div>
-                        <button type="submit" className="btn btn-default btn-sm btn-success"
-                            onClick={() => this.onUpdateGroup(item.id)}><FaPen />
-                        </button>
-                        <span> </span>
-                        <button className="btn btn-default btn-sm btn-danger" onClick={() => this.onDeleteGroup(item)}><FaTrash />
-                        </button>
-                    </div>
-                </th>
-            </tr>
-        )
-        return <div>
-            {
-                <section className="panel">
-                    <h5 className="panel-heading">Listado de Grupos</h5>
-                    <button className="new-group-item btn btn-primary btn-sm"
-                        onClick={this.onCreateGroup}> Nuevo Grupo
-                        </button>
-                    <div className="panel-body">
-                        {(itemslist.length > 0) ? (<div>
-                            <Table>
-                                <thead className="text-info">
-                                    <tr>
-                                        <th>Grupo</th>
-                                        <th>Profesor</th>
-                                        <th>Acciones</th>
-                                    </tr>
-                                </thead>
-                                <tbody >{itemslist}</tbody>
-                            </Table></div>
-                        ) : <p className="text-danger">No existen grupos, haga click en el botón Nuevo grupo</p>}
-                    </div>
-                </section>
-            }
-        </div>
-    }
-
-    render() {
-        return (
-            <div className="container">
-                {this.state.isCreating || this.state.isUpdating ? '' : this.renderDefaultview()}
-                {this.state.isCreating ? this.renderCreateview() : ''}
-                {this.state.isUpdating ? this.renderUpdateview(this.state.id) : ''}
+              ) : (
+                <p className="text-danger">
+                  No existen grupos, haga click en el botón Nuevo grupo
+                </p>
+              )}
             </div>
-        )
-    }
-}
+          </section>
+        }
+      </div>
+    );
+  };
 
-export default GroupList
+  <div className="container">
+    {isCreating || isUpdating
+      ? ""
+      : renderDefaultview()}
+    {isCreating ? renderCreateview() : ""}
+    {isUpdating ? renderUpdateview(id) : ""}
+  </div>;
+};
+
+export default GroupList;
